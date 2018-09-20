b648bc1e8a684a99000ea53f155cf67256a9f7b0
Al Mondragon <al.drago@live.com>
2018-09-20 00:24:37 -0500

    Updated info

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ import Skills from '../components/Skills';
 import PostLoop from '../components/PostLoop';
 import FrontpageContact from '../components/Frontpage/Contact';
 
-import BombEmoji from '../assets/img/emoji/bomb.png';
+import OkEmoji from '../assets/img/emoji/ok.png';
 import TeachingEmoji from '../assets/img/emoji/teaching.png';
 import AlMonPortrait from '../assets/img/avatar-face-400px.png';
 
@@ -29,10 +29,10 @@ export default class Frontpage extends Component {
             <div className="About pt2">
                 <div className="container Frontpage__hero">
                     <h1>
-                        Mi nombre es <span className="text blue">Alberto Mondragón</span> <img src={ BombEmoji } alt="Bomb emoji" />
+                        Mi nombre es <span className="text blue">Alberto Mondragón</span>
                     </h1>
                     <h3>
-                        Me encanta el desarrollo web <span className="text blue">Front-End</span>
+                        Me encanta el desarrollo web <span className="text blue">Front-End</span> <img src={OkEmoji} alt="Ok emoji" />
                     </h3>
                 </div>
                 <div className="container About__bio">
@@ -43,13 +43,15 @@ export default class Frontpage extends Component {
 
                     <aside className="bio">
                         <p>
-                            I've been working as a designer and developer for <strong>over 15 years now</strong>, with over a decade of 
-                            experience in the cannabis industry. 
+                        He sido <strong className="text blue">diseñador y desarrollador web de Front-End</strong> por 5 años, me encanta aprender y 
+                        adaptarme a los constantes cambios que caracterizan al campo del Web Development.
                         </p>
                         <p>
-                            From engineering <a href="http://kushy.net">web apps</a> and e-commerce solutions, 
-                            to designing brands and products, to producing innovative digital media, and even creating <strong>famous <a href="http://weedporndaily.tumblr.com/tagged/gif">ganja related GIFs</a></strong> -- my experience 
-                            as an <strong>entrepreneur</strong> has broadened my skillset substantially.
+                        He trabajado desarrollando <strong className="text blue">apps</strong> y websites para una serie de industrias variadas, desde el <strong className="text blue">e-commerce</strong> hasta 
+                        los <strong className="text blue">anuncios</strong> e incluso <strong className="text blue">bienes raíces</strong> y muchas más.
+                        </p>
+                        <p> 
+                        Me <strong className="text blue">apasiona</strong> mi profesión y me encanta pertenecer a equipos dinámicos y afrontar nuevos <strong className="text blue">retos</strong> para extender mis habilidades.
                         </p>
                     </aside>
                 </div>
